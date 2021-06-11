db553db82c825d1f1d86100cbb3a56c6dcf80d01
ivana <ivananl@yahoo.com>
2021-06-11 16:01:31 +0200

    Add user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: { registrations: 'registrations' }
+  root to: 'events#index'
   resources :events
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
 end
