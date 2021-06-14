7350d493e92489f715485f78495cf7108c3fffbe
Moises Hernandez <mhdezcoronado@gmail.com>
2021-06-14 16:41:59 +0200

    Update the Event’s Show

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,7 @@ class Event < ApplicationRecord
     belongs_to :creator, class_name: 'User'
     has_many :attended_events, foreign_key: 'attended_id'
     has_many :visitors, :through => :attended_events
+
+    scope :past_events, -> { where('Date < ?', Date.today )}
+    scope :future_events, -> { where('Date >= ?', Date.today )}
 end
